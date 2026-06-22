54bf90e02108636123fa5b3d3742cbbd2c56de15
Hampus <hampe_king1@hotmail.com>
2026-06-22 18:33:15 +0200

    Final badges

--- src/MangaHub/pbconfig.ts
+++ src/MangaHub/pbconfig.ts
@@ -14,6 +14,7 @@ export default {
     SourceIntents.CLOUDFLARE_BYPASS_PROVIDING,
     SourceIntents.DISCOVER_SECTION_PROVIDING,
     SourceIntents.SEARCH_RESULT_PROVIDING,
+    SourceIntents.SETTINGS_FORM_PROVIDING,
   ],
   badges: [],
   developers: [
