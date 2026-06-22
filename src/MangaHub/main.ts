ee361bd0944e18d69b9e466d2262785498b75885
Hampus <hampe_king1@hotmail.com>
2026-06-22 16:35:11 +0200

    id for dedupication missed in search?

--- src/MangaHub/main.ts
+++ src/MangaHub/main.ts
@@ -468,7 +468,7 @@ export class MangaHubExtension implements MangaHubImplementation {
     const offset = (page - 1) * PER_PAGE;
     const gql = `{
       search(x:${this.mangaSource},q:${JSON.stringify(queryText)},genre:${JSON.stringify(genre)},mod:${order},offset:${offset}) {
-        rows { title author slug image genres latestChapter }
+        rows { id title author slug image genres latestChapter }
       }
     }`;
     const result = await this.graphQL(gql);
